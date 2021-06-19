bb867dd6d3b89ac69b91d5e8cdbb378b89558eb8
suharshirajapaksha <73971127+suharshirajapaksha@users.noreply.github.com>
2021-06-20 02:55:30 +0530

    Updates

---
+++ precache-manifest.5129d2db22b578a53415ca82eef56f77.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99009ba236427cd6c007a799e08b135c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f25ee2b532618cd66f49",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "cba1460700875d6db14f",
+    "url": "/static/js/2.da8ed2da.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.da8ed2da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f25ee2b532618cd66f49",
+    "url": "/static/js/main.3b1529af.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "81efc9fc8d982e4b997ca8b0f5f8b586",
+    "url": "/static/media/resume.81efc9fc.pdf"
+  },
+  {
+    "revision": "a18df0c65c7423037f686e6c38d8e501",
+    "url": "/static/media/suharshirajapaksha.a18df0c6.jpg"
+  }
+]);
